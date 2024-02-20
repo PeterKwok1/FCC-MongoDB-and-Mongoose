2c9d708ef0d27cce058d7dbe668f7155168571b2
Peter Kwok <peter.kwok94@gmail.com>
2024-02-19 23:09:33 -0500

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
+let mongoose = require("mongoose")
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-let Person;
+const Schema = mongoose.Schema
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
